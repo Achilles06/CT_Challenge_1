8285d7fb6a01705c2ad094a9a0614e88126f06fc
Achilles06 <92751219+Achilles06@users.noreply.github.com>
2024-04-08 21:39:06 -0400

    Add files via upload

---
+++ pythonSyntax.py
@@ -0,0 +1,78 @@
+
+#1. Showcase Your Dance Moves
+# Task 1: Code Correction
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2: Your
+today = input("How do you feel today?")
+
+if today == "happy":
+    print("That's great to hear!")
+elif today == "sad":
+    print("I hope your day gets better!")
+
+#3. Python Naming Convention Practice
+#Task 1: Code Correction
+pi_value = 3.14
+user_age = 25
+user_location = "New Your"
+max_limit = 1000
+
+#4. Arithmetic Operations in Daily Life
+#Task 1: Grocery Store Math
+
+bread_cost = 2.15
+peanutbutter_cost = 5.09
+jelly_cost = 3.25
+
+grocerylist_cost = bread_cost + peanutbutter_cost + jelly_cost
+
+print("Total cost of grocery list is $", grocerylist_cost)
+
+#Task 2: Bank Interest
+
+savings_account = 10000
+yearly_interest = 7
+
+savings_account = savings_account + (savings_account * yearly_interest) / 100 
+
+print(savings_account)
+
+#5.Understanding Assignments and Comparisons
+#Task 1: Value Swapping 
+
+value_one = 5
+value_two = 10
+
+value_temp = value_one
+value_one = value_two
+value_two = value_temp
+
+print(value_one, value_two)
+
+#6. Exploring Logical Operations and Precedence
+#Task 1: Validating Calculations
+expression_one = 5+2
+expression_two = (5+2)
+
+print("Expression one =", expression_one)
+print("Expression two =", expression_two)
+
+if expression_one == expression_two:
+    print("Both expressions equal the same.")
+else: 
+    print("The expressions are not equal.")
+
+#Task 2: Mix and Match
+var_1 = 15
+var_2 = 7
+var_3 = 2
+
+answer = var_1+var_2 > var_3
+#True
+print(answer)
